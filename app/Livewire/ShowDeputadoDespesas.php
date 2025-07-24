c171d9d62a23d6405ed47c970c46659314e405f3
alef-thallys <alef.thallys22@gmail.com>
2025-07-24 16:47:36 -0300

    feat: create Livewire component to display deputado despesas

---
+++ app/Livewire/ShowDeputadoDespesas.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Deputado;
+use Livewire\Component;
+
+class ShowDeputadoDespesas extends Component
+{
+    public Deputado $deputado;
+
+    public function mount(Deputado $deputado)
+    {
+        $this->deputado = $deputado->load([
+            'despesas' => function ($query) {
+                $query->orderBy('ano', 'desc')
+                    ->orderBy('mes', 'desc');
+            }
+        ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.show-deputado-despesas');
+    }
+}
