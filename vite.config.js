ba50b3b6ee97a4d166b43a4e237a265ea9a42617
alef-thallys <alef.thallys22@gmail.com>
2025-07-25 12:49:59 -0300

    feat: update docker-compose and Vite config for improved server accessibility

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -10,4 +10,11 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
